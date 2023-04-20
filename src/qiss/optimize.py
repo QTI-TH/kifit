74e9ad296702d6f2ca0c9365f139b02f2da57835
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-04-20 11:48:26 +0200

    first script drafts

---
+++ src/qiss/optimize.py
@@ -0,0 +1,37 @@
+
+
+def npmodel(parameters):
+    """
+    Returns the model output, i.e. mv2 = k + F * mv2 + alpha X gamma
+    
+    Args:
+        parameters (array):  parameters matrix containg all the problem's parameters.
+        Each row corresponds to a scatterplot and contains slope, intercept, and
+        new physics coefficient for a given couple of isotopes.
+    """
+
+    
+
+
+
+
+class Optimizer:
+    """
+    Parent optimizer class. 
+    It returns the best set of parameters.
+    """
+
+    _method = None
+
+    def __init__(datapath):
+        """
+        Args: 
+            datapath (str): path to target data.
+        """
+
+
+
+    def loss()
+        """
+        
+        """
