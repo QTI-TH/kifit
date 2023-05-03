7744adae7cbe10e6bcb9ff1626b1b3faaf8b2578
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-05-03 13:10:40 +0200

    further improv to otpimizer

--- src/qiss/optimize.py
+++ src/qiss/optimize.py
@@ -9,18 +9,15 @@ import cma
 def npmodel(par, data):
     """
     Model calculation, i.e. mv2 = k + F * mv1 + alpha X gamma
-    
-    Args:
-        par (array):  parameters matrix containg all the problem's parameters.
-        Each row corresponds to a scatterplot and contains slope, intercept, and
-        new physics coefficient for a given couple of isotopes.
-        data (matrix): each raw represents a set of target x-axis data. 
-    
+
     Returns: all set of y-axis predictions
     """
+    pass
 
-    return par[0] + par[1] * data + par[2]
 
+def dummy_model(par, x):
+    # THIS IS ONLY A DUMMY MODEL USED FOR TESTS
+    return par[0]*x + par[1] + par[1]*np.log(x)
 
 
 class Optimizer:
@@ -33,46 +30,61 @@ class Optimizer:
         Args: 
             datapath (str): path to target data folder.
         """
-        path = Path(datapath)
-        self._data =  np.load(path/"nu.dat") 
-        #self._g = np.load(path/"g.dat")        TO FIX
-
-        self._dimensions = self._data.shape
-
-        # first matrix column as fixed abscissa
-        self._x = self.data.T[0]
-        # other columns as y variables
-        self._y = self.data.T[1:]
-
-        self._alpha = 0
-        # (2 x N-1), with N number of data columns
-        self._params = np.zeros((2, self._dimensions[1] - 1))
-
-        # optimizer options
-        self._options = {}
-
-
-    def linear_fit(self):
-        """
-        Linear fit without new physics.
         
-        Returns: list of intercepts and slopes associated to each mv2 mv1 combo.
-        """
+        # loading data
+        self._data, self._g, self._x, self._npx, self._y = self.load_data(datapath)
+        
+        # dimensions and initial parameters setting
+        self._dimensions = self._data.shape
+        self._alpha = 0
+        self._params = np.zeros((self._dimensions[1] - 1, 2))
+
+
+    def load_data(self, datapath):
+        """Loads data to be used during the optimization."""
+
+        path = Path(datapath)
+
+        data =  np.load(path/"nu.dat") 
+        g = np.load(path/"g.dat") 
+        npx = np.load(path/"npx.dat")    
+        x = self._data.T[0]
+        y = self._data.T[1:]
+
+        return data, g, x, npx, y
+    
+    
+    def set_reference_index(self, index):
+        """Sets the new target index used as mv1 in each scatterplot"""
+
+        self._x = self._data.T[index]
+        self._y = np.delete(self._data, index, axis=1)
+        # TO FIX:  what about g?
+
+    
+    def linear_fit(self):
+        """updates linear fitting parameters."""
 
         lin_params = []
-
         for y in self._y:
             lin_params.append(sps.linregress(self._x, y))
 
-        return lin_params
+        self._params = np.asarray(lin_params)
+
 
 
     def loss(self, data, params):
         """ Calculate loss function."""
+        # THE FOLLOWING LOSS IS ONLY FOR TESTING
+        
+        
 
-        # sum over MSE residual \forall isotopes couples
+        for i, y in enumerate(self._y):
+            prediction = 
+
         return 
     
+
     @abstractmethod
     def set_options(self, **kwargs):
         """Cast the options passed into the format expected by the optimizer"""
@@ -80,17 +92,18 @@ class Optimizer:
 
 
     def optimize(self, initial_p):
+        """
+        Optimization method which must be implemented according to each optimizer.
+        
+        Return: tuple [best loss function value, best parameters, full optimizer
+        results dictionary].  
+        """
         if self._method is None:
             raise ValueError(
                 f"The optimizer {self.__class__.__name__} does not implement any methods"
             )
-        return minimize(self.loss, initial_p, method=self._method, options=self._options)
-    
-    def set_reference_index(self, index):
-        """Sets the new target index used as mv1 in each scatterplot"""
+        pass
 
-        self._x = self._data.T[index]
-        self._y = np.delete(self._data, index, axis=1)
     
 
 class CMA(Optimizer):
@@ -110,21 +123,14 @@ class CMA(Optimizer):
         }
 
 
-    def optimize(self, initial_p):
+    def optimize(self, initial_parameters):
         """Calls the cma optimizer"""
-
-        # TO DO
-    
-
-
-class BFGS(Optimizer):
-    """
-    Calls the scipy's Broyden-Fletcher-Goldfarb-Shanno (BFGS) optimizer.
-    """
-
-    _method = "bfgs"
-
-    def set_options(self, **kwargs):
-        self._options = {"disp": True, "return_all": True}
-        print(f"Initial parameters: {self._predictor.parameters}")
-    
+        
+        res = cma.fmin2(
+            loss=self.loss,
+            initial_parameters=initial_parameters,
+            options=self._options,
+            args=()
+        )
+        
+        return res[1].result.fbest, res[1].result.xbest, res    
