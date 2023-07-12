b76b0c9152b6908a697f0458fc45e7a6e3533951
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-07-12 10:48:10 +0200

    CMA and ScipyMinimizer implemented

--- src/qiss/optimize.py
+++ src/qiss/optimize.py
@@ -1,129 +1,55 @@
-from abc import abstractmethod
 import numpy as np
-from pathlib import Path
-from dataclasses import dataclass
-import os
 
 import scipy.stats as sps
 from scipy.optimize import minimize
 import cma
 
-def npmodel(par, data):
-    """
-    Model calculation, i.e. mv2 = k + F * mv1 + alpha X gamma
-
-    Returns: all set of y-axis predictions
-    """
-    pass
-
-
-def dummy_model(par, x):
-    # THIS IS ONLY A DUMMY MODEL USED FOR TESTS
-    return par[0]*x + par[1] + par[1]*np.log(x)
-
-
-@dataclass
-class Element:
-    """Each element should be associated to an Element class"""
-    name: str
-    ndata: int
-    x: np.ndarray
-    y: np.ndarray
-    g: np.ndarray
-    npx: np.ndarray
-    parameters: np.ndarray
-
 
 class Optimizer:
     """General optimizer class, which returns the best set of parameters."""
 
-    _method = None
-
-    def __init__(self, datapath):
+    def __init__(
+            self, 
+            datapath: str, 
+            target_loss: float, 
+            max_iterations: int, 
+            verbose: int = 1):
         """
         Args: 
             datapath (str): path to target experiment folder.
                 each experiment folder must be organised with one subfolder for
                 each element involved into the Kings Plot fitting.
+            target_loss (float): target loss function value at which the optimization
+                can be stopped.
+            max_iterations (int): maximum number of iterations.
+            verbose (int): verbosity level increasing from -1 (quiet) to +1 (verbose).
         """
+        self._method = None
+        self.datapath = datapath
+        self.target_loss = target_loss
+        self.max_iterations = max_iterations
+        self.verbose = verbose
+        self.loss = self.build_loss_function()
+
+    def optimize(self):
+        """
+        Perform the optimization strategy according to the chose method.
         
-        # parent path to the experiment folder
-        self._path = Path(datapath)
-        self._elements = []
-
-        for element_name in os.listdir(self._path):
-            # loading data from element folder
-            data, g, x, npx, y = self.load_data(element_name)
-            # appending to _elements list the new element class
-            self._elements.append(Element(
-                name=element_name,
-                ndata=data.shape[0],
-                x=x,
-                y=y,
-                g=g,
-                npx=npx
-            ))
-
-
-    def load_data(self, element_name):
-        """Load data to be used during the optimization."""
-
-        element_path = self._path/element_name
-
-        data =  np.load(element_path/"nu.dat") 
-        g = np.load(element_path/"g.dat") 
-        npx = np.load(element_path/"npx.dat")    
-        
-        x = self._data.T[0]
-        y = self._data.T[1:]
-
-        return data, g, x, npx, y
+        Returns:
+            Best loss function value registered.
+            Best set of parameters.
+            OptimizerResult object which depends on the used method.
+        """
+        raise NotImplementedError("Subclasses must implement the optimize() method.")
     
-    
-    def set_reference_index(self, index):
-        """Set the new target index used as mv1 in each scatterplot"""
+    def build_loss_function(self):
+        """Build loss function using data of a target experiment."""
+        pass
 
-        self._x = self._data.T[index]
-        self._y = np.delete(self._data, index, axis=1)
-        # TO FIX:  what about g?
-
-    
     def linear_fit(self):
-        """Update linear fitting parameters."""
-
-        lin_params = []
-        for y in self._y:
-            lin_params.append(sps.linregress(self._x, y))
-
-        self._params = np.asarray(lin_params)
-
-
-
-    def loss(self, data, params):
-        """ Calculate loss function."""
-
-        for elem in self._elements:
-            #use elem.data for calculating the loss function!
-        pass
-    
-
-    @abstractmethod
-    def set_options(self, **kwargs):
-        """Cast the options passed into the format expected by the optimizer"""
-        pass
-
-
-    def optimize(self, initial_p):
-        """
-        Optimization method which must be implemented according to each optimizer.
-        
-        Return: tuple [best loss function value, best parameters, full optimizer
-        results dictionary].  
-        """
-        if self._method is None:
-            raise ValueError(
-                f"The optimizer {self.__class__.__name__} does not implement any methods"
-            )
+        """Perform linear regression."""
+        # use sps.linregress(x, y)
+        # ideally on each combination of x and y we need to take into account    
         pass
 
     
@@ -133,26 +59,115 @@ class CMA(Optimizer):
     Call a CMA-ES optimizer. 
     Ref: https://arxiv.org/abs/1604.00772 
     """
-    _method = "cma"
 
-    def set_options(self, **kwargs):
-        self._options = {
-            "verbose": -1,
-            "tolfun": 1e-12,
-            "ftarget": kwargs["tol_error"],  # target error
-            "maxiter": kwargs["max_iterations"],  # maximum number of iterations
-            "maxfeval": kwargs["max_evals"],  # maximum number of function evaluations
+    def __init__(
+            self, 
+            datapath : str,
+            target_loss : float, 
+            max_iterations : int, 
+            initial_params : list, 
+            maxfeval : int, 
+            verbose : int = 1):
+        """
+        Args:
+            datapath: path to target experiment folder.
+                each experiment folder must be organised with one subfolder for
+                each element involved into the Kings Plot fitting.
+            target_loss: target loss function value at which the optimization
+                can be stopped.
+            max_iterations: maximum number of iterations.
+            verbose: if True, log messages are printed during the optimization.
+            initial_params: initial guess of paramameters.
+            maxfeval: maximum number of function evaluations.
+        """
+        super().__init__(datapath, target_loss, max_iterations, verbose)
+        
+        self._method = "cma"
+        self.initial_params = initial_params
+        self.maxfeval = maxfeval
+
+
+    def optimize(self) -> tuple[float, list]:
+        """Call the CMA-ES optimizer."""
+
+        options = {
+            "verbose": self.verbose,
+            "tolfun": self.target_loss,
+            "maxiter": self.max_iterations,
+            "maxfeval": self._maxfeval
         }
-
-
-    def optimize(self, initial_parameters):
-        """Calls the cma optimizer"""
         
         res = cma.fmin2(
             loss=self.loss,
-            initial_parameters=initial_parameters,
-            options=self._options,
-            args=()
+            initial_parameters=self.initial_params,
+            args=(),
+            options = options
         )
         
-        return res[1].result.fbest, res[1].result.xbest, res    
+        return res[1].result.fbest, res[1].result.xbest, res
+    
+
+class ScipyMinimizer(Optimizer):
+    """
+    Call scipy.optimize.minimize method. 
+    Official documentation at: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
+    """
+
+    def __init__(
+            self, 
+            datapath : str,
+            target_loss : float, 
+            max_iterations : int, 
+            initial_params : list, 
+            method: str = "BFGS",
+            jac: callable = None,
+            hess: callable = None,
+            hessp: callable = None,
+            bounds: tuple = None,
+            costraints: dict = None,
+            callback: callable = None,
+            options: dict = None,
+            verbose : int = 1):
+        """
+        Args:
+            datapath: path to target experiment folder.
+                each experiment folder must be organised with one subfolder for
+                each element involved into the Kings Plot fitting.
+            target_loss: target loss function value at which the optimization
+                can be stopped.
+            max_iterations: maximum number of iterations.
+            verbose: if True, log messages are printed during the optimization.
+            initial_params: initial guess of paramameters.
+            maxfeval: maximum number of function evaluations.
+            # FIXME --> write missing documentation.
+        """
+        super().__init__(datapath, target_loss, max_iterations, verbose)
+
+        self._method = f"scipy_optimizer_{method}"
+        self.initial_params = initial_params
+        self.method = method
+        self.jac = jac
+        self.hess = hess
+        self.hessp = hessp
+        self.bounds = bounds
+        self.costraints = costraints
+        self.callback = callback
+        self.options = options
+
+    def optimize(self):
+        """Execute the minimization according to the chosen method."""
+        res = minimize(
+            fun=self.loss,
+            x0=self.initial_params,
+            args=(),
+            method=self.method,
+            jac=self.jac,
+            hess=self.hess,
+            hessp=self.hessp,
+            bounds=self.bounds,
+            constraints=self.costraints,
+            tol=self.tol,
+            callback=self.callback,
+            options=self.options
+        )
+        return res.fun, res.x, res
