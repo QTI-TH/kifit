c246047255f7a2b6e90b2091f576be09b87c5176
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-04-05 13:45:34 +0200

    refactor: working main with iterative_mc_search

--- src/kifit/main.py
+++ src/kifit/main.py
@@ -1,20 +1,17 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-from kifit import performfit
 from kifit.loadelems import Elem
-from kifit.plotfit import plot_loss_varying_alphaNP
+from kifit.performfit import (
+    iterative_mc_search
+    )
+from kifit.plotfit import plot_linfit, plot_alphaNP_ll, plot_mphi_alphaNP
 
-nsamples = 20000
-elem = Elem.get("Ca_WT_Aarhus_PTB_2021")
-elem.sig_alphaNP_init = 1e-9
+ca = Elem.get('Ca')
+ca.sig_alphaNP_init = 1e-5
 
-# elem._update_Xcoeffs(400)
+print(ca.get_dimensions)
 
-element_samples = performfit.generate_element_sample(elem, nsamples)
-alphaNP_list, ll_list, ba, _, best_alpha = performfit.iterative_mc_search(
-    elem=elem, element_samples=element_samples, niter=5, delta_alpha_ratio=0.4
+result = iterative_mc_search(
+    elem=ca,
+    n_sampled_elems=2000,
+    mphivar=False,
+    niter=20
 )
-plot_loss_varying_alphaNP(alphaNP_list=alphaNP_list, ll_list=ll_list)
-
-print(f"\n\nBest alpha value: {ba}")
