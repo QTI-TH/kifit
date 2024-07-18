d0318c765c2c3d48036da4ce3ecf9e3f8b5672d2
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-07-18 16:29:09 +0400

    modified script

--- src/kifit/run.sh
+++ src/kifit/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=kifit
+#SBATCH --job-name=kifit_es
 #SBATCH --output=kifit_%A_%a.log
-#SBATCH --array=1-3
+#SBATCH --array=1-5
 
 
-num_searches_list=(5 10 20)
-num_searches=${num_searches_list[$SLURM_ARRAY_TASK_ID-1]}
+# num_searches_list=(5 10 20 50 100 500 1000)
+# num_searches=${num_searches_list[$SLURM_ARRAY_TASK_ID-1]}
 
-# num_elemsamples_list=(50 100 200 500 1000)
-# num_elemsamples=${num_elemsamples_list[$SLURM_ARRAY_TASK_ID-1]}
+num_elemsamples_list=(50 100 200 500 1000)
+num_elemsamples=${num_elemsamples_list[$SLURM_ARRAY_TASK_ID-1]}
 
-python3 main.py --outputfile_name "testing_bounds_${SLURM_ARRAY_TASK_ID}" \
+python3 main.py --outputfile_name "nelems_${SLURM_ARRAY_TASK_ID}" \
                 --elements_list "Yb_Kyoto_MIT_GSI_PTB_2024,Ca_WT_Aarhus_PTB_2024" \
                 --optimization_method "Powell" \
-                --maxiter 1000 --num_searches $num_searches --num_elemsamples_search 200 \
-                --num_experiments 5  --block_size  5 \
-                --num_alphasamples_exp 200 --num_elemsamples_exp 200 \
+                --maxiter 1000 --num_searches 20 --num_elemsamples_search $num_elemsamples \
+                --num_experiments 10  --block_size  5 \
+                --num_alphasamples_exp 200 --num_elemsamples_exp $num_elemsamples \
                 --num_samples_det 100 --random_seed 42 --x0 0 \
                      
                      
